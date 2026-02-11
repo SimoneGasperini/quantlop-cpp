7fce70ebec14f7f1c4b2ab0a29e23b8749b1b74d
SimoneGasperini <simone.gasperini4@unibo.it>
2026-02-11 12:06:17 +0100

    Extend benchmarks to krylov method

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -6,14 +6,15 @@ import pylab as plt
 with open("runtime.json", "r") as file:
     data = json.load(file)
 
-lab1, lab2, lab3 = data.keys()
-col1, col2, col3 = "tab:blue", "tab:orange", "tab:green"
+lab1, lab2, lab3, lab4 = data.keys()
+col1, col2, col3, col4 = "tab:blue", "tab:orange", "tab:green", "tab:purple"
 alpha = 0.4
 yticks = [0.01, 0.1, 1, 10, 100]
 qubits = sorted(int(i) for i in data[lab3])
 runtime1 = np.array([data[lab1][str(q)] for q in qubits if str(q) in data[lab1]])
-runtime2 = np.array([data[lab2][str(q)] for q in qubits if str(q) in data[lab2]])
+runtime2 = np.array([data[lab2][str(q)] for q in qubits])
 runtime3 = np.array([data[lab3][str(q)] for q in qubits])
+runtime4 = np.array([data[lab4][str(q)] for q in qubits])
 
 fig, ax = plt.subplots(figsize=(9, 6))
 ax.plot(
@@ -26,17 +27,20 @@ ax.plot(
 for k in range(runtime1.shape[1]):
     ax.plot(qubits[: len(runtime1)], runtime1[:, k], color=col1, alpha=alpha)
 ax.plot(
-    qubits[: len(runtime2)],
+    qubits,
     np.mean(runtime2, axis=1),
     marker="s",
     label=lab2,
     color=col2,
 )
 for k in range(runtime2.shape[1]):
-    ax.plot(qubits[: len(runtime2)], runtime2[:, k], color=col2, alpha=alpha)
+    ax.plot(qubits, runtime2[:, k], color=col2, alpha=alpha)
 ax.plot(qubits, np.mean(runtime3, axis=1), marker="s", label=lab3, color=col3)
 for k in range(runtime3.shape[1]):
     ax.plot(qubits, runtime3[:, k], color=col3, alpha=alpha)
+ax.plot(qubits, np.mean(runtime4, axis=1), marker="s", label=lab4, color=col4)
+for k in range(runtime4.shape[1]):
+    ax.plot(qubits, runtime4[:, k], color=col4, alpha=alpha)
 ax.set_xlabel("Qubits", fontsize=18)
 ax.set_xticks(qubits)
 ax.set_xticklabels(qubits, fontsize=14)
@@ -57,8 +61,9 @@ with open("memory.json", "r") as file:
 
 yticks = [0.1, 1, 10, 100, 1000]
 memory1 = np.array([data[lab1][str(q)] for q in qubits if str(q) in data[lab1]])
-memory2 = np.array([data[lab2][str(q)] for q in qubits if str(q) in data[lab2]])
+memory2 = np.array([data[lab2][str(q)] for q in qubits])
 memory3 = np.array([data[lab3][str(q)] for q in qubits])
+memory4 = np.array([data[lab4][str(q)] for q in qubits])
 
 fig, ax = plt.subplots(figsize=(9, 6))
 ax.plot(
@@ -66,12 +71,15 @@ ax.plot(
 )
 for k in range(memory1.shape[1]):
     ax.plot(qubits[: len(memory1)], memory1[:, k], color=col1, alpha=alpha)
-ax.plot(qubits[: len(memory2)], np.mean(memory2, axis=1), marker="s", label=lab2, color=col2)
+ax.plot(qubits, np.mean(memory2, axis=1), marker="s", label=lab2, color=col2)
 for k in range(memory2.shape[1]):
-    ax.plot(qubits[: len(memory2)], memory2[:, k], color=col2, alpha=alpha)
+    ax.plot(qubits, memory2[:, k], color=col2, alpha=alpha)
 ax.plot(qubits, np.mean(memory3, axis=1), marker="s", label=lab3, color=col3)
 for k in range(memory3.shape[1]):
     ax.plot(qubits, memory3[:, k], color=col3, alpha=alpha)
+ax.plot(qubits, np.mean(memory4, axis=1), marker="s", label=lab4, color=col4)
+for k in range(memory4.shape[1]):
+    ax.plot(qubits, memory4[:, k], color=col4, alpha=alpha)
 ax.set_xlabel("Qubits", fontsize=18)
 ax.set_xticks(qubits)
 ax.set_xticklabels(qubits, fontsize=14)
--- benchmark/run.py
+++ benchmark/run.py
@@ -31,9 +31,14 @@ def scipy_sparse_simulation(nq, op, psi):
     return expm_multiply(-1j * sparse, psi, traceA=0)
 
 
-def quantlop_simulation(nq, op, psi):
+def quantlop_higham_simulation(nq, op, psi):
     linop = Hamiltonian.from_pennylane(op, num_qubits=nq)
-    return evolve(linop, psi)
+    return evolve(linop, psi, method="higham")
+
+
+def quantlop_krylov_simulation(nq, op, psi):
+    linop = Hamiltonian.from_pennylane(op, num_qubits=nq)
+    return evolve(linop, psi, method="krylov")
 
 
 def runtime_and_memory(func, *args, reps, interval=0.0005):
@@ -56,14 +61,24 @@ def runtime_and_memory(func, *args, reps, interval=0.0005):
 
 
 def run_benchmark(num_qubits, time_fname, mem_fname, reps):
-    runtime = {"SciPy dense": {}, "SciPy sparse": {}, "QuantLop": {}}
-    memory = {"SciPy dense": {}, "SciPy sparse": {}, "QuantLop": {}}
+    runtime = {
+        "SciPy dense": {},
+        "SciPy sparse": {},
+        "QuantLop (higham)": {},
+        "QuantLop (krylov)": {},
+    }
+    memory = {
+        "SciPy dense": {},
+        "SciPy sparse": {},
+        "QuantLop (higham)": {},
+        "QuantLop (krylov)": {},
+    }
     for nq in num_qubits:
         print(f"\nRunning simulation for {nq} qubits:")
         op = get_rand_hamiltonian(nqubits=nq, num_terms=5 * nq)
         psi = np.zeros(2**nq, dtype=complex)
         psi[0] = 1
-        if nq < 15:
+        if nq < 8:
             time, mem, result1 = runtime_and_memory(
                 scipy_dense_simulation, nq, op, psi, reps=reps
             )
@@ -75,13 +90,19 @@ def run_benchmark(num_qubits, time_fname, mem_fname, reps):
         runtime["SciPy sparse"][nq] = time
         memory["SciPy sparse"][nq] = mem
         time, mem, result3 = runtime_and_memory(
-            quantlop_simulation, nq, op, psi, reps=reps
+            quantlop_higham_simulation, nq, op, psi, reps=reps
         )
-        runtime["QuantLop"][nq] = time
-        memory["QuantLop"][nq] = mem
-        if nq < 15:
+        runtime["QuantLop (higham)"][nq] = time
+        memory["QuantLop (higham)"][nq] = mem
+        time, mem, result4 = runtime_and_memory(
+            quantlop_krylov_simulation, nq, op, psi, reps=reps
+        )
+        runtime["QuantLop (krylov)"][nq] = time
+        memory["QuantLop (krylov)"][nq] = mem
+        if nq < 8:
             assert np.allclose(result1, result2)
         assert np.allclose(result2, result3)
+        assert np.allclose(result3, result4)
         with open(time_fname, "w") as file:
             json.dump(runtime, file, indent=4)
         with open(mem_fname, "w") as file:
@@ -90,7 +111,7 @@ def run_benchmark(num_qubits, time_fname, mem_fname, reps):
 
 if __name__ == "__main__":
     run_benchmark(
-        num_qubits=range(1, 23),
+        num_qubits=range(1, 21),
         time_fname="runtime.json",  # sec
         mem_fname="memory.json",  # MB
         reps=7,
