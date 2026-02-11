00739d42fb42acc65f890e771c4c13ec017e75e9
SimoneGasperini <simone.gasperini4@unibo.it>
2026-02-11 10:54:38 +0100

    Implement krylov method in C++

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -7,19 +7,21 @@ from quantlop import get_rand_hamiltonian
 
 
 @pytest.mark.parametrize("num_qubits", range(1, 9))
-def test_scipy(num_qubits):
+@pytest.mark.parametrize("method", ("higham", "krylov"))
+def test_scipy(num_qubits, method):
     psi = np.zeros(2**num_qubits, dtype=complex)
     psi[0] = 1
     op = get_rand_hamiltonian(nqubits=num_qubits, num_terms=num_qubits * 5)
     mat = op.matrix(range(num_qubits))
     psi_scipy = sp.linalg.expm(-1j * mat) @ psi
     ham = Hamiltonian.from_pennylane(op, num_qubits=num_qubits)
-    psi_linop = evolve(ham, psi, method="higham")
+    psi_linop = evolve(ham, psi, method=method)
     assert np.allclose(psi_scipy, psi_linop)
 
 
 @pytest.mark.parametrize("num_qubits", range(1, 11))
-def test_pennylane(num_qubits):
+@pytest.mark.parametrize("method", ("higham", "krylov"))
+def test_pennylane(num_qubits, method):
     psi = np.zeros(2**num_qubits, dtype=complex)
     psi[0] = 1
     op = get_rand_hamiltonian(nqubits=num_qubits, num_terms=num_qubits * 5)
@@ -31,5 +33,5 @@ def test_pennylane(num_qubits):
 
     psi_pennylane = circuit()
     ham = Hamiltonian.from_pennylane(op, num_qubits=num_qubits)
-    psi_linop = evolve(ham, psi, method="higham")
+    psi_linop = evolve(ham, psi, method=method)
     assert np.allclose(psi_pennylane, psi_linop)
